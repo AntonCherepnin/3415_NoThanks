7c55e81993c53837e6ce7297b78adaed40bba8b9
Anton <antoncherepnin347@gmail.com>
2024-09-16 15:07:26 +0300

    Update

--- test/test_card.py
+++ test/test_card.py
@@ -5,6 +5,12 @@ from card import (Card)
 def test_init():
     a = Card(15)
     assert a.value == 15
+    with pytest.raises(ValueError):
+        Card('e')
+    with pytest.raises(ValueError):
+        Card(2.21)
+    with pytest.raises(ValueError):
+        Card(1)
 def test_repr():
     a = Card(12)
     assert a.__repr__() == '15'
