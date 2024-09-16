fb832df49a04a20e79a92208cee2b3ebf64b257a
Anton <antoncherepnin347@gmail.com>
2024-09-16 06:00:41 +0300

    Update README.md

--- test/test_card.py
+++ test/test_card.py
@@ -1,2 +1,11 @@
-class card:
-    pass
+import pytest
+from src.card import card
+
+
+def test_init():
+    c = Card(15)
+    assert c.value == 12
+def test_repr():
+    c = Card(12)
+    assert c.__repr__() == '15'
+    assert Card.__repr__(Card(13)) == '13'
